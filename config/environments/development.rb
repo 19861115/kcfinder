3d88058a3a1c423e78239865e601242a3a468964
19861115 <ipewlm@hotmail.co.jp>
2016-03-09 22:04:29 +0900

    誤ってbulletでgrowlを使う設定をしてしまっていたので削除

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,6 @@ Rails.application.configure do
     Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
-    Bullet.growl = true
     Bullet.rails_logger = true
   end
 end
